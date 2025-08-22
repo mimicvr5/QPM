e3f1939921c6b8a81957e9fb0cb3951e8d7b068f
Hamm797 <jack.cannon@stu.boone.kyschools.us>
2025-08-22 17:10:46 -0400

    Added "exit" command

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter a words >> ");
-        String hi = scanner.nextLine();
-        System.out.println(hi);
+        System.out.println("Q+- V1.0:");
+
+        String line;
+        while (true) {
+            System.out.print("> ");
+            line = scanner.nextLine();
+            if (Objects.equals(line, "exit")) {
+                break;
+            }
+            System.out.println(line);
+        }
     }
 }
